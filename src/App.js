6ef18623b11f4da03b251868fc14513c813ab55a
Elias-FS <elias-fausto@hotmail.com>
2022-08-09 17:59:14 -0300

    added auth5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
-function App() {
+function App({ signOut, user }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Teste de atualização</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quos vel, perspiciatis ex voluptate odit veniam dolore accusantium debitis alias ab repellendus, asperiores autem culpa? Minima voluptates architecto deserunt error.</p>
-      </header>
+      {user.attributes.email}
+      <button onClick={signOut}>Sign out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {socialProviders: ['apple','google']});
