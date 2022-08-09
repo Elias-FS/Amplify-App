3d999e069ba786be89e6186a9fa8f18d37caa21c
Elias-FS <elias-fausto@hotmail.com>
2022-08-09 08:55:56 -0300

    added auth4

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Amplify } from 'aws-amplify';
 
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
 import awsExports from './aws-exports';
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
