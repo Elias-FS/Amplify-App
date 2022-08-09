5f028785ffad6cd30f68ce8b977bd50ee2b2dad2
Elias-FS <elias-fausto@hotmail.com>
2022-08-09 18:10:09 -0300

    added auth6

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 import '@aws-amplify/ui-react/styles.css'
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import awsmobile from './aws-exports';
 import { AmplifyProvider } from '@aws-amplify/ui-react'
-Amplify.configure(config);
+Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
